b914356e6437e0545a72956b52c848acba6f7e91
Drew Gibbs <drewdoesgamemaker@gmail.com>
2016-11-16 20:21:06 -0500

    save

--- dodge2/dodge.js
+++ dodge2/dodge.js
@@ -12,9 +12,12 @@ var C = {
   },
   "p": {
     "file": "assets/player.png",
-    "width": 46,
-    "height": 64,
-    "frames": 2
+    "width": 40,
+    "height": 40,
+    "frames": 2,
+    "startx": 0,
+    "yspeed": 700,
+    "file": "assets/background.png"
   }
 }
 //( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)
@@ -33,7 +36,7 @@ class Boot {
     preload() {
       console.log("Loading...");
        this.load.image("bg",C.bg.file)
-       this.load.spritesheet("player",C.p.file,C.p.width,C.p.height,C.p.frames)
+       this.load.spritesheet("player",C.p.file,C.p.width,C.p.height,C.p.frames);
     }
     create() {
       console.log("Loaded");
@@ -47,6 +50,10 @@ class Boot {
       console.log("Entered Play State");
       this.background = this.add.tileSprite(0,0,C.bg.width,C.bg.height,"bg");
       this.background.autoScroll(C.bg.xspeed,C.bg.yspeed);
+      this.player = this.add.sprite(C.p.startx,C.p.starty,"player");
+      this.player.anchor.set(0.5,0.5);
+      this.player.smoothed = true;
+      this.player.scale.set(1);
     }
   }
 
